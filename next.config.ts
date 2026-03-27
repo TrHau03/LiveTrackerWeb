4e8d95e787731906bbf1881a117f83f806a4ec27
ANHDUY <duyzxje2110@gmail.com>
2026-03-27 17:44:51 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // Nếu bạn có dùng thẻ <Image /> của Next.js, bạn cần bỏ comment dòng bên dưới
+  // images: { unoptimized: true }
 };
 
 export default nextConfig;
